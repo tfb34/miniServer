495e7657700a7487a26d665ca8ea65ff08ff8a47
Thalia <thalia.brito@rutgers.edu>
2019-12-04 01:27:10 -0500

    fix typo

--- database/connection.js
+++ database/connection.js
@@ -9,9 +9,9 @@ const Sequelize = require("sequelize");
 
 const sequelize;
 
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+if (process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+    sequelize = new Sequelize(DATABASE_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
       port:     match[4],
