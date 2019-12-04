64f5987d0d2d2c174a05f4c7ea99520ca7bed767
Thalia <thalia.brito@rutgers.edu>
2019-12-04 01:47:45 -0500

    Fix connection

--- database/connection.js
+++ database/connection.js
@@ -10,7 +10,7 @@ var sequelize;
  
 if (process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
-    sequelize = new Sequelize(DATABASE_URL, {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
       dialect:  'postgres',
       protocol: 'postgres',
       port:     match[4],
