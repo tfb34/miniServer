90e369bdd2d5ce15b8dcee1783e47855bdc73884
Thalia <thalia.brito@rutgers.edu>
2019-12-04 01:00:04 -0500

    Setup sequelize

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,38 @@
 const Sequelize = require("sequelize");
 
-const sequelize= new Sequelize("simplepos_db","root","password",{
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    operatorsAliases: false
-});
+// const sequelize= new Sequelize("simplepos_db","root","password",{
+//     host: '127.0.0.1',
+//     dialect: 'mysql',
+//     operatorsAliases: false
+// });
+
+
+const sequelize;
+
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    sequelize = new Sequelize("simplepos_db","root","password",{
+        host: '127.0.0.1',
+        dialect: 'mysql',
+        operatorsAliases: false
+    });
+  }
+
 
 module.exports = sequelize;
 global.sequelize = sequelize;
+
+
+//postgres://yivembiwyjmhdz:3318b12315046efe96735e8b6cca492d5caf9da42ae37fa0685353078dbe3bcb@ec2-54-225-72-238.compute-1.amazonaws.com:5432/d8m18giasgs34u
+
+
+
